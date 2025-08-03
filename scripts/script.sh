45cccc121a63ed761cdf2d6bc613654771bf66a2
zayneshzangar <zangarzaynesh@gmail.com>
2025-08-03 16:58:50 +0500

    change script in create-db.sh. Delete row where get grant superuser

--- scripts/script.sh
+++ scripts/script.sh
@@ -4,12 +4,12 @@
 if [[ "$1" == "--delete" ]]; then
     echo "Удаление базы данных..."
     source $(pwd)/delete-psql.sh
-    sleep 5
-    source $(pwd)/delete-kafka.sh
+    # sleep 5
+    # source $(pwd)/delete-kafka.sh
 else
     echo "Создание и запуск базы данных..."
     $(pwd)/run-psql.sh
     sleep 5
     $(pwd)/create-db.sh
-    $(pwd)/run-kafka.sh
+    # $(pwd)/run-kafka.sh
 fi
